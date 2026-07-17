0a377bf3e8e451a5db0e46f9304602dac3e04520
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2026-07-17 11:05:57 +0800

    init

---
+++ src/client/java/com/ericsson/toggledifficultyfeatures/client/ToggleDifficultyFeaturesClient.java
@@ -0,0 +1,10 @@
+package com.ericsson.toggledifficultyfeatures.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToggleDifficultyFeaturesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
